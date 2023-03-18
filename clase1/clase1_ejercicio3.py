b322f5cc2c591e4d4024a1ec102e654188585baf
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-17 22:02:24 -0300

    Create clase1_ejercicio3.py

---
+++ clase1/clase1_ejercicio3.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('words')
+nltk.download('brown')
+
+from nltk.corpus import brown
+from random import sample
+list_of_words = list(brown.words())
+
+n = 1000000
+rand_words = sample(list_of_words, n)
+df = pd.DataFrame(rand_words,columns=["word"])
