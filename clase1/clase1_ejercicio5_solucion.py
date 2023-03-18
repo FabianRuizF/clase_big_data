e9ca923120d1837588ce000a999b5c70f9723d87
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-17 23:27:06 -0300

    Update clase1_ejercicio5_solucion.py

--- clase1/clase1_ejercicio5_solucion.py
+++ clase1/clase1_ejercicio5_solucion.py
@@ -1,7 +1,11 @@
 import multiprocessing.dummy as mp
+import numpy as np
+import functools
+
+list_of_numbers = np.arange(10000000)
 
 %%timeit -n 10 -r 3
 
 with mp.Pool(processes=4) as p:
   pow_number = p.map(cube, list_of_numbers,chunksize=2000000)
-sum(pow_number)
+print(functools.reduce(add, pow_number))
