7e391217936403e0f2f537bc5105cfd9cf56f8c4
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-25 00:15:52 -0300

    Update ejercicio3_solucion.py

--- clase3/ejercicio3_solucion.py
+++ clase3/ejercicio3_solucion.py
@@ -6,4 +6,4 @@ spark = SparkSession.builder.master('spark://139.144.62.82:7077').getOrCreate()
 
 from pyspark.mllib.linalg import DenseVector
 from pyspark.mllib.random import RandomRDDs
-data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).map(lambda a : a.tolist()).toDF()
+data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).sum()
