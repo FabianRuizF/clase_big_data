c3bd15906b9f2fd4443a126f9a2f9d16e522df79
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-25 00:21:29 -0300

    Update and rename ejercicio6_solucion.py to ejercicio7_solucion.py

--- clase3/ejercicio7_solucion.py
+++ clase3/ejercicio7_solucion.py
@@ -4,7 +4,6 @@ from pyspark import SparkConf
 
 spark = SparkSession.builder.master('spark://139.144.62.82:7077').getOrCreate()
 
-from pyspark.mllib.linalg import DenseVector
 from pyspark.mllib.random import RandomRDDs
 data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).map(lambda a : a.tolist()).toDF()
 print(data.summary().show())
