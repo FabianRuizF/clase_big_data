48ce755246652dcb7bbf9c82cba7f6c23f277ef0
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-25 00:20:58 -0300

    Create ejercicio6_solucion.py

---
+++ clase3/ejercicio6_solucion.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+
+
+spark = SparkSession.builder.master('spark://139.144.62.82:7077').getOrCreate()
+
+from pyspark.mllib.linalg import DenseVector
+from pyspark.mllib.random import RandomRDDs
+data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).map(lambda a : a.tolist()).toDF()
+print(data.summary().show())
