b27060f12ebb2d761c25da4fefb628d9bdcaa19e
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-31 23:26:09 -0300

    Update ejercicio9_solucion.py

--- clase3/ejercicio9_solucion.py
+++ clase3/ejercicio9_solucion.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder.master('spark://139.144.62.82:7077').appName('Calc_
 
 from pyspark.mllib.linalg import DenseVector
 from pyspark.mllib.random import RandomRDDs
-data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 100000000,6,100).map(lambda a : a.tolist()).toDF()
+data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 100000000,6,100).map(lambda a : a.tolist()).toDF(["num1","num2","num3","num4","num5","num6"])
 
 exprs = {x: "sum" for x in data.columns }
 print(exprs)
