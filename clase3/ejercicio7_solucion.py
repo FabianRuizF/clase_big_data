2cd54b8f922c82b2dce8c65c05ac95da95554561
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-03-31 23:14:42 -0300

    Update ejercicio7_solucion.py

--- clase3/ejercicio7_solucion.py
+++ clase3/ejercicio7_solucion.py
@@ -5,5 +5,5 @@ from pyspark import SparkConf
 spark = SparkSession.builder.master('spark://139.144.62.82:7077').getOrCreate()
 
 from pyspark.mllib.random import RandomRDDs
-data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).map(lambda vector : vector.tolist()).toDF()
+data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, 10000,6,6).map(lambda vector : vector.tolist()).toDF(["num1","num2","num3","num4","num5","num6"])
 print(data.summary().show())
