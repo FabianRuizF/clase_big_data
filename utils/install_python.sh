f3b0fccfe8b0518ac3c7600d4e49ee0c6960de89
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-04 11:54:03 -0400

    Update install_python.sh

--- utils/install_python.sh
+++ utils/install_python.sh
@@ -1,6 +1,6 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
 bash ~/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh -b -p $HOME/miniconda
-$CONDA_EXE init
+$HOME/miniconda/bin/conda init
 source ~/.bashrc
 
 pip install pyspark==3.3.2
