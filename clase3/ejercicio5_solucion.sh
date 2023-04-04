0ae8d5263d260b55faab439723fc0114a5898c8f
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-04 11:09:32 -0400

    Update ejercicio5_solucion.sh

--- clase3/ejercicio5_solucion.sh
+++ clase3/ejercicio5_solucion.sh
@@ -1,4 +1,4 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
-bash Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
+bash ~/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh -b -p $HOME/miniconda
 pip install pyspark==3.3.2
 pip install pandas
