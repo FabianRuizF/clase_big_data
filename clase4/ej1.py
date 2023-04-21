c00d9d4a617d47a7765dcbe007fdeb884e3f5fab
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-21 11:36:00 -0400

    Update ej1.py

--- clase4/ej1.py
+++ clase4/ej1.py
@@ -6,4 +6,5 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.master('spark://139.144.62.82:7077').config("spark.driver.memory", "2g").config("spark.executor.memory", "2g").getOrCreate()
 
 data = spark.read.csv("/mnt/nfs_share/test.csv")
-data.show()
+print(data.count())
+data.show(
