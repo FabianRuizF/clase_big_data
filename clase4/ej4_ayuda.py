e8f6dc9df287f39168685242cee980cc76440dad
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-21 12:26:37 -0400

    Create ej4_ayuda.py

---
+++ clase4/ej4_ayuda.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+my_ip = s.getsockname()[0]
+
+
+
+
+byte_to_use= ### buscar una cantidad adecuada a usar
+part_num = 1660
+spark = SparkSession.builder.master('spark://139.144.62.82:7077').appName('CalcDataFrame').config("spark.sql.files.minPartitionNum",part_num).config("spark.sql.files.maxPartitionBytes",  int(###rellenar)).config("spark.default.parallelism", part_num).config("spark.driver.host",my_ip).config("spark.driver.memory", "2g").config("spark.executor.memory", "2g").getOrCreate()
+
+
+data = spark.read.parquet("/mnt/nfs_share/data/central_west.parquet")
+
+data.printSchema()
+print(data.count())
