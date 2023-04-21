4c1966ff70b31f27ec8947c52994b05d76a7e053
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2023-04-21 12:19:49 -0400

    Create ej3_solucion.py

---
+++ clase4/ej3_solucion.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+my_ip = s.getsockname()[0]
+
+
+
+part_num = 80
+spark = SparkSession.builder.master('spark://139.144.62.82:7077').appName('CalcDataFrame').config("spark.sql.files.minPartitionNum",part_num).config("spark.default.parallelism", part_num).config("spark.driver.host",my_ip).config("spark.driver.memory", "2g").config("spark.executor.memory", "2g").getOrCreate()
+
+
+
+
+data = spark.read.parquet("/mnt/nfs_share/data/central_west.parquet")
+
+data.printSchema()
+print(data.count())
